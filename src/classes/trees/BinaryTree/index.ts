d216af4da8885b8ca565d1cdab704ae07984b8f0
Tomer Groisman <tomergroisman@gmail.com>
2021-12-04 19:28:57 +0200

    Add a binary tree class

---
+++ src/classes/trees/BinaryTree/index.ts
@@ -0,0 +1,166 @@
+/**
+ * Class of a binary tree representation
+ */
+export class BinaryTree<T> {
+  private _tree: (T | null)[];
+  private _nodes: number[];
+
+  /**
+   * A binary tree class constructor
+   * @param {(T | null)[]} tree An array representation of a binary tree (optional)
+   */
+  constructor(tree?: (T | null)[]) {
+    this._tree = tree ?? [];
+    this._nodes = this._getNodeNumToIndexMap();
+  }
+
+  /**
+   * Returns the number of nodes of the tree
+   * @return {number} The size of the tree (number of nodes)
+   */
+  get size(): number {
+    return this._nodes.length;
+  }
+
+  /**
+   * Returns the parent of a node or null if the node is the root
+   * @param {number} nodeNum A node index number
+   * @return {T | null} The value of the parent node
+   * @throws {RangeError} NodeNum is not a valid node
+   */
+  parent(nodeNum: number): T | null {
+    const nodeIndex = this._nodeNumToIndex(nodeNum);
+    const parentIndex = Math.floor((nodeIndex - 1) / 2);
+    return this._tree[parentIndex] ?? null;
+  }
+
+  /**
+   * Returns the left chide of a node or null if the left child is missing
+   * @param {number} nodeNum A node index number
+   * @return {T | null} The value of the left child node
+   * @throws {RangeError} NodeNum is not a valid node
+   */
+  leftChild(nodeNum: number): T | null {
+    const nodeIndex = this._nodeNumToIndex(nodeNum);
+    const leftChildIndex = ((nodeIndex + 1) * 2) - 1;
+    return this._tree[leftChildIndex] ?? null;
+  }
+
+  /**
+   * Returns the right chide of a node or null if the right child is missing
+   * @param {number} nodeNum A node index number
+   * @return {T | null} The value of the right child node
+   * @throws {RangeError} NodeNum is not a valid node
+   */
+  rightChild(nodeNum: number): T | null {
+    const nodeIndex = this._nodeNumToIndex(nodeNum);
+    const rightChildIndex = (nodeIndex + 1) * 2;
+    return this._tree[rightChildIndex] ?? null;
+  }
+
+  /**
+   * Returns the depth of a node
+   * @param {number} nodeNum A node index number
+   * @return {number} The depth of the node
+   * @throws {RangeError} NodeNum is not a valid node
+   */
+  depth(nodeNum: number): number {
+    const nodeIndex = this._nodeNumToIndex(nodeNum);
+    return this._depthOfIndex(nodeIndex);
+  }
+
+  /**
+   * Returns true if the node is a valid node, false otherwise
+   * @param {number} nodeNum A node index number
+   * @return {boolean} True of the node is a valid node, false otherwise
+   */
+  isNode(nodeNum: number): boolean {
+    return this._nodes[nodeNum] !== undefined;
+  }
+
+  /**
+   * Returns true if the node is a leaf, false otherwise
+   * @param {number} nodeNum A node index number
+   * @return {boolean} True if the node is a leaf, false otherwise
+   * @throws {RangeError} NodeNum is not a valid node
+   */
+  isLeaf(nodeNum: number): boolean {
+    const hasNoLeftChild = !this.leftChild(nodeNum);
+    const hasNoRightChild = !this.rightChild(nodeNum);
+    return hasNoLeftChild && hasNoRightChild;
+  }
+
+  /**
+   * Returns true if the tree index is a valid tree index, false otherwise
+   * @param {number} index The tree index
+   * @return {boolean} True if the tree index is a valid tree index, false otherwise
+   */
+  _isIndexValid(index: number): boolean {
+    return 0 <= index && index > this._tree.length;
+  }
+
+  /**
+   * Returns a tree index number of this node index number
+   * @param {number} nodeNum A node index number
+   * @return {number} The tree index of this node index number
+   * @throws {RangeError} NodeNum is not a valid node
+   */
+  _nodeNumToIndex(nodeNum: number): number {
+    if (this.isNode(nodeNum)) {
+      return this._nodes[nodeNum];
+    }
+    throw new RangeError('Node number is out of range');
+  }
+
+  /**
+   * Generate a map from node index to tree index (ignoring null nodes)
+   * @return {number[]} An array map from node index to tree index
+   */
+  _getNodeNumToIndexMap(): number[] {
+    return this._tree.reduce<number[]>((prev, current, i) => {
+      if (current !== null) {
+        prev.push(i);
+      }
+      return prev;
+    }, []);
+  }
+
+  /**
+   * Returns the depth of an index
+   * @param {number} index A tree index
+   * @return {number} The depth of the tree index
+   * @throws {RangeError} NodeNum is not a valid node
+   */
+  _depthOfIndex(index: number): number {
+    if (this._isIndexValid(index)) {
+      throw new RangeError('Index number is out of range');
+    }
+    return Math.floor(Math.log2(index + 1));
+  }
+
+  /**
+   * Returns rue if the node is tree index is last in the depth level
+   * @param {number} index The index of the tree index
+   * @return {boolean} True if the node is tree index is last in the depth level
+   */
+  _isLastInDepthLevel(index: number): boolean {
+    const depth = this._depthOfIndex(index);
+    return depth !== this._depthOfIndex(index + 1);
+  }
+
+  /**
+   * Returns a string representation of the tree
+   * @return {string} A string representation of the tree
+   */
+  toString(): string {
+    return this._tree.reduce((prev, current, i) => {
+      prev += current;
+      prev; // ?
+      this._isLastInDepthLevel(i);
+      if (this._isLastInDepthLevel(i)) {
+        return prev + '\n';
+      }
+      return prev + ' ';
+    }, '').slice(0, -1);
+  }
+}
--- src/classes/trees/BinaryTree/indx.test.ts
+++ src/classes/trees/BinaryTree/indx.test.ts
@@ -1,6 +1,132 @@
+import {BinaryTree} from '.';
+
 describe('Binary Tree Tests', () => {
   it('should initialize a new binary tree', () => {
     const tree = new BinaryTree();
     expect(tree).toBeDefined();
+    expect(tree.size).toBe(0);
+  });
+
+  it('should initialize a new binary tree from a pre defined tree', () => {
+    const tree = new BinaryTree([1, 2, null, 3]);
+    expect(tree).toBeDefined();
+    expect(tree.size).toBe(3);
+  });
+
+  it('should print the tree', () => {
+    const tree = new BinaryTree([
+      0,
+      1, 2,
+      3, 4, null, 5,
+      null, null, 6, 7, null, null, null, null,
+      null, null, null, null, 8,
+    ]);
+    const expectation = (
+      '0\n1 2\n3 4 null 5\nnull null 6 7 null null null null\nnull null null null 8'
+    );
+    expect(tree.toString()).toBe(expectation);
+  });
+
+  describe('Tree Node Tests', () => {
+    let tree: BinaryTree<number>;
+
+    beforeEach(() => {
+      /**
+       *      0
+       *     / \
+       *    1   2
+       *   / \   \
+       *  3   4   5
+       *     / \
+       *    6   7
+       *   /
+       *  8
+       */
+      tree = new BinaryTree(
+          [
+            0,
+            1, 2,
+            3, 4, null, 5,
+            null, null, 6, 7, null, null, null,
+            null, null, null, null, null, 8,
+          ],
+      );
+    });
+
+    it('should return node children', () => {
+      expect(tree.leftChild(0)).toBe(1);
+      expect(tree.rightChild(0)).toBe(2);
+
+      expect(tree.leftChild(1)).toBe(3);
+      expect(tree.rightChild(1)).toBe(4);
+
+      expect(tree.leftChild(2)).toBeNull();
+      expect(tree.rightChild(2)).toBe(5);
+
+      expect(tree.leftChild(3)).toBeNull();
+      expect(tree.rightChild(3)).toBeNull();
+
+      expect(tree.leftChild(4)).toBe(6);
+      expect(tree.rightChild(4)).toBe(7);
+
+      expect(tree.leftChild(5)).toBeNull();
+      expect(tree.rightChild(5)).toBeNull();
+
+      expect(tree.leftChild(6)).toBe(8);
+      expect(tree.rightChild(6)).toBeNull();
+
+      expect(tree.leftChild(7)).toBeNull();
+      expect(tree.rightChild(7)).toBeNull();
+
+      expect(tree.leftChild(8)).toBeNull();
+      expect(tree.rightChild(8)).toBeNull();
+
+      expect(() => tree.rightChild(9)).toThrow(RangeError);
+    });
+
+    it('should return node parents', () => {
+      expect(tree.parent(0)).toBeNull();
+
+      expect(tree.parent(1)).toBe(0);
+      expect(tree.parent(2)).toBe(0);
+
+      expect(tree.parent(3)).toBe(1);
+      expect(tree.parent(4)).toBe(1);
+
+      expect(tree.parent(5)).toBe(2);
+
+      expect(tree.parent(6)).toBe(4);
+      expect(tree.parent(7)).toBe(4);
+
+      expect(tree.parent(8)).toBe(6);
+
+      expect(() => tree.parent(9)).toThrow(RangeError);
+    });
+
+    it('should return true for leafs anf false for non leaves', () => {
+      expect(tree.isLeaf(0)).toBeFalsy();
+      expect(tree.isLeaf(1)).toBeFalsy();
+      expect(tree.isLeaf(2)).toBeFalsy();
+      expect(tree.isLeaf(3)).toBeTruthy();
+      expect(tree.isLeaf(4)).toBeFalsy();
+      expect(tree.isLeaf(5)).toBeTruthy();
+      expect(tree.isLeaf(6)).toBeFalsy();
+      expect(tree.isLeaf(7)).toBeTruthy();
+      expect(tree.isLeaf(8)).toBeTruthy();
+      expect(() => tree.isLeaf(9)).toThrow(RangeError);
+    });
+
+    it('should return the depth of the nodes', () => {
+      expect(tree.depth(0)).toBe(0);
+      expect(tree.depth(1)).toBe(1);
+      expect(tree.depth(2)).toBe(1);
+      expect(tree.depth(3)).toBe(2);
+      expect(tree.depth(4)).toBe(2);
+      expect(tree.depth(5)).toBe(2);
+      expect(tree.depth(6)).toBe(3);
+      expect(tree.depth(7)).toBe(3);
+      expect(tree.depth(8)).toBe(4);
+      expect(() => tree.isLeaf(9)).toThrow(RangeError);
+    });
   });
 });
